0f43da6964a6db0ddc0fdb0a5edad0c972efb8d2
faishal-ai <okbos6337@gmail.com>
2025-10-01 12:29:04 +0700

    Floating Action Button (FAB)

--- Pertemuan_05/hello_world/lib/main.dart
+++ Pertemuan_05/hello_world/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
+// Langkah 2: Buat Widget MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,24 +13,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'My Increment App'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+// Langkah 3: Buat Stateful Widget MyHomePage
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// Langkah 4: Buat State untuk MyHomePage
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // Fungsi untuk menambah counter
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -46,23 +51,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(height: 50.0),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment Counter',
+        tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
