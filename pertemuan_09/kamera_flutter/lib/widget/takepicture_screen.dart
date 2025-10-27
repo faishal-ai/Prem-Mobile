32f0a3046788f9cf8974befecd98cf8235997dd8
faishal-ai <okbos6337@gmail.com>
2025-10-27 12:08:14 +0700

    p1

---
+++ pertemuan_09/kamera_flutter/lib/widget/takepicture_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'displaypicture_screen.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({super.key, required this.camera});
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // Langkah 4: Inisialisasi controller kamera
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Langkah 5 & 6: Preview kamera + tombol ambil foto
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture - NIM Anda')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Jika kamera siap → tampilkan preview
+            return CameraPreview(_controller);
+          } else {
+            // Jika belum siap → tampilkan loading
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+
+            if (!context.mounted) return;
+
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            print('Terjadi error: $e');
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
